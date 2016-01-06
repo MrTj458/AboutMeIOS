814ce92af4d40a0192210d1d0c1c97c0c7530ab6
Trevor <thod0127@740s-pl-130.local>
2016-01-06 12:16:01 -0700

    Added controllers for the views

---
+++ AboutMeIOS/VideoGameViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VideoGameViewController.swift
+//  AboutMeIOS
+//
+//  Created by Hodsdon, Trevor on 1/6/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VideoGameViewController : UIViewController
+{
+    
+}
